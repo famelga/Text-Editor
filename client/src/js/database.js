eaaabb6762446d132e37eef6eafe0c0b673856a9
Fayven <fayven.amelga@gmail.com>
2023-03-02 23:57:34 -0500

    Remove StaleWhileRevalidate

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -26,9 +26,9 @@ export const putDb = async (content) => {
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.log("GET from the database");
-  const contactDb = await openDB("contact", 1);
-  const tx = contactDb.transaction("contact", "readonly");
-  const store = tx.objectStore("contact");
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readonly");
+  const store = tx.objectStore("jate");
   const request = store.getAll();
   const result = await request;
   console.log("result.value", result);
